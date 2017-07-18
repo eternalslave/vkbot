e3f031575dc67f4746669819fdfed732c01ab34c
eternalslave <dark.fusrodah@gmail.com>
2017-07-18 00:25:49 +0000

    edit

--- rpgbeta.py
+++ rpgbeta.py
@@ -17,9 +17,11 @@ def status(id):
     else:
         r=file.read()
         file.close()
-        return r
+        return str(r)
 def write_msg(s):
     vk.method('messages.send', {'chat_id':1,'message':s})
+def gifts(id):
+    return '\nGifts\n———————————————\n'
 while True:
     response = vk.method('messages.get', values)
     if response['items']:
@@ -76,7 +78,7 @@ while True:
             file=open('gld/'+str(item['user_id'])+'.txt', 'r')
             gold=file.read()
             file.close()
-            write_msg(u'Профиль @id'+str(item['user_id'])+' (' + vk.method('users.get', { 'user_ids':item['user_id']})[0]['first_name']+ u')\nLVL: '+str(int(math.log(int(exp)/5+1, 2)))+emoji.emojize(':bust_in_silhouette:\nexp: ')+str(int(exp))+'/'+str((2**(int(math.log(int(exp)/5+1, 2))+1)-1)*5)+emoji.emojize(':books:\nGold: ')+gold[4:]+emoji.emojize(':money_with_wings:')+'\nStatus: '+status(item['user_id']))
+            write_msg(u'Профиль @id'+str(item['user_id'])+' (' + vk.method('users.get', { 'user_ids':item['user_id']})[0]['first_name']+ u')\nLVL: '+str(int(math.log(int(exp)/5+1, 2)))+emoji.emojize(':bust_in_silhouette:\nexp: ')+str(int(exp))+'/'+str((2**(int(math.log(int(exp)/5+1, 2))+1)-1)*5)+emoji.emojize(':books:\nGold: ')+gold[4:]+emoji.emojize(':money_with_wings:')+'\nStatus: '+status(item['user_id'])+gifts(item['user_id']))
         if item['body']=='/kit':
             try:
                 file=open('gld/'+str(item['user_id'])+'_kit.txt', 'r')
