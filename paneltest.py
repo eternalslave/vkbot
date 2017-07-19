0bc0eb7aca7c83c0972c40fd08939c8a2de9184c
Eternal Slave <dark.fusrodah@gmail.com>
2017-07-19 05:48:08 +0300

    admin fix

--- paneltest.py
+++ paneltest.py
@@ -16,46 +16,56 @@ def panel():
         if response['items']:
             values['last_message_id'] = response['items'][0]['id']
         for item in response['items']:
-            if item['body']=='/admin' and item['user_id']==32191511:
-                write_msg('1) Кик \n 2) Отмена \n darkness inside')
-                go=True
-                while go:
-                    response = bot.method('messages.get', values)
-                    if response['items']:
-                        values['last_message_id'] = response['items'][0]['id']
-                    for item1 in response['items']:
-                        if item1['user_id']==32191511 and item1['body']=='1':
-                            write_msg('погоди чуток..')
-                            lst=adm.method('messages.getChatUsers', {'chat_id':340})
-                            num=1
-                            usersSTR=u'Выбери из списка чел)\n'
-                            users=[]
-                            for id in lst:
-                                user=bot.method('users.get', {'user_ids':id})
-                                users.append(user[0]['id'])
-                                usersSTR=usersSTR+str(num)+') '+user[0]['first_name']+' '+user[0]['last_name']+', '
-                                num+=1
-                            write_msg(usersSTR)
-                            while go:
-                                response = bot.method('messages.get', values)
-                                if response['items']:
-                                    values['last_message_id'] = response['items'][0]['id']
-                                for item2 in response['items']:
-                                    if item2['user_id']==32191511 and item2['body']=='/stop':
-                                        go=False
-                                        write_msg('ok)')
-                                        break
-                                    if item2['user_id']==32191511 and int(item2['body'], 10)>=0 and int(item2['body'], 10)<num:
-                                        adm.method('messages.removeChatUser', {'chat_id':340, 'user_id':users[int(item2['body'], 10)-1]})
-                                        write_msg('ббалити из реалити')
-                                        break
-                            break
-                                    
-                        if item1['user_id']==32191511 and item1['body']=='2':
-                            write_msg('ok)')
-                            go=False
-                            break
-                        if item1['user_id']==32191511:
-                            write_msg(u'Выберите пункт')
-panel()
+            if item['body']=='/admin':
+                try:
+                    open('status/'+str(item['user_id'])+'.txt')
+                except IOError as e:
+                    write_msg('Вы не админ')
+                    pass
+                else:
+                    idd=item['user_id']
+                    status=open('status/'+str(idd)+'.txt')
+                    stat=status.read()
+                    if stat=='Admin':
+                        write_msg('1) Kick \n 2) Отмена')
+                        go=True
+                        while go:
+                            response = bot.method('messages.get', values)
+                            if response['items']:
+                                values['last_message_id'] = response['items'][0]['id']
+                            for item1 in response['items']:
+                                if item1['user_id']==idd and item1['body']=='1':
+                                    write_msg('Загружается список..')
+                                    lst=adm.method('messages.getChatUsers', {'chat_id':340})
+                                    num=1
+                                    usersSTR=u'Выберите пользователя\n'
+                                    users=[]
+                                    for id in lst:
+                                        user=bot.method('users.get', {'user_ids':id})
+                                        users.append(user[0]['id'])
+                                        usersSTR=usersSTR+str(num)+') '+user[0]['first_name']+' '+user[0]['last_name']+', '
+                                        num+=1
+                                    write_msg(usersSTR)
+                                    while go:
+                                        response = bot.method('messages.get', values)
+                                        if response['items']:
+                                            values['last_message_id'] = response['items'][0]['id']
+                                        for item2 in response['items']:
+                                            if item2['user_id']==idd and item2['body']=='/stop':
+                                                go=False
+                                                write_msg('Завершено')
+                                                break
+                                            if item2['user_id']==idd and int(item2['body'], 10)>=0 and int(item2['body'], 10)<num:
+                                                adm.method('messages.removeChatUser', {'chat_id':340, 'user_id':users[int(item2['body'], 10)-1]})
+                                                write_msg('Пользователь удален')
+                                                break
+                                    break
+                                            
+                                if item1['user_id']==idd and item1['body']=='2':
+                                    write_msg('Завершено')
+                                    go=False
+                                    break
+                                if item1['user_id']==idd:
+                                    write_msg(u'Выберите пункт')
+        panel()
 
